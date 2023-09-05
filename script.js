d19caba75ec12888b22f19e6d40b5f74213ed28e
shyamsundar89 <128958648+shyamsundar89@users.noreply.github.com>
2023-09-05 18:10:48 +0530

    Remove Unnecessary Commented code

--- script.js
+++ script.js
@@ -2,18 +2,10 @@
 const quotes = document.getElementById("quotes");
 const author = document.getElementById("author");
 const newQuote = document.getElementById("new");
-// const Tweet = document.getElementById("tweet");
 let realData = "";
-
-// const tweetNow = () => {
-//     let tweetPost = `https://twitter.com/intent/tweet?text=${quotesData.text}`;
-//     window.open(tweetPost);
-// }
 
 const getNewQuotes = () => {
     let rendomNum = Math.floor(Math.random() * realData.length);
-    // console.log(realData[rendomNum].author);
-    // console.log(rendomNum);
     let quotesData = realData[rendomNum]
     quotes.innerText = `${quotesData.text}`;
     if(quotesData.author == null) {
@@ -29,15 +21,11 @@ const getQuotes = async () => {
     try{
         let data = await fetch(api);
         realData = await data.json();
-        // console.log(realData.length);
         getNewQuotes();
-        // console.log(realData[0].text);
-        // console.log(realData[0].author);
     } catch(error) {
 
     }
 };
 
 newQuote.addEventListener("click", getNewQuotes);
-// Tweet.addEventListener("click", tweetNow);
 getQuotes();
